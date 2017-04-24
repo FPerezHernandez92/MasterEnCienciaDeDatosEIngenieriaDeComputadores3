ae9e46c475b60c573b55d90d181a4385cd9e047f
PacoPollos <herpefran92@MacBook-Pro-de-Francisco.local>
2017-04-24 23:23:31 +0200

    STMFD: Minería de flujos de datos, cambios en los scripts

--- 4 Series temporales y mineria de flujos de datos/script.sh
+++ 4 Series temporales y mineria de flujos de datos/script.sh
@@ -1,6 +1,48 @@
+#!/bin/bash
 
-#./script2.1.sh >> salida2.1.txt
-#./script2.2.sh >> salida2.2.txt
-#./script2.3.sh >> salida2.3.txt
-./script2.4.sh >> salida2.4.txt
-#./script2.5.sh >> salida2.5.txt
+for ejercicio in `seq 1 2`
+do
+	for SEMILLA in `seq 1 5`
+	do
+		echo -e "\n\nEl valor de la semilla es: " $SEMILLA >> $ejercicio
+		if [ $ejercicio -eq 1 ]
+		then
+			echo -e "\nPrimero HoeffdingTree: " >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "EvaluateModel -m (LearnModel -l trees.HoeffdingTree -s (generators.WaveformGenerator -i $SEMILLA) -m 1000000) -s (generators.WaveformGenerator -i 4)" >> $ejercicio	
+			echo -e "\nAhora HoeffdingAdaptativeTree" >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "EvaluateModel -m (LearnModel -l trees.HoeffdingAdaptiveTree -s (generators.WaveformGenerator -i $SEMILLA) -m 1000000) -s (generators.WaveformGenerator -i 4)" >> $ejercicio
+		fi
+
+		if [ $ejercicio -eq 2 ]
+		then
+			echo -e "\nPrimero HoeffdingTree: " >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "EvaluateInterleavedTestThenTrain -l moa.classifiers.trees.HoeffdingTree -s (generators.WaveformGenerator -i $SEMILLA) -i 1000000 -f 10000" >> $ejercicio	
+			echo -e "\nAhora HoeffdingAdaptativeTree" >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "EvaluateInterleavedTestThenTrain -l moa.classifiers.trees.HoeffdingAdaptiveTree -s (generators.WaveformGenerator -i $SEMILLA) -i 1000000 -f 10000" >> $ejercicio
+		fi
+
+		if [ $ejercicio -eq 3 ]
+		then
+			echo -e "\nPrimero HoeffdingTree: " >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio	
+			echo -e "\nAhora HoeffdingAdaptativeTree" >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio
+		fi
+
+		if [ $ejercicio -eq 4 ]
+		then
+			echo -e "\nPrimero HoeffdingTree: " >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio	
+			echo -e "\nAhora HoeffdingAdaptativeTree" >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio
+		fi
+
+		if [ $ejercicio -eq 5 ]
+		then
+			echo -e "\nPrimero HoeffdingTree: " >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio	
+			echo -e "\nAhora HoeffdingAdaptativeTree" >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio
+		fi
+	done
+done
