b51a8cc63a3520e49757ae963e54519bf84baeb2
FPerezHernandez92 <herpefran92@correo.ugr.es>
2017-05-03 11:46:48 +0200

    STMFD: Práctica sobre flujos de datos acabada

--- 4 Series temporales y mineria de flujos de datos/script.sh
+++ 4 Series temporales y mineria de flujos de datos/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ejercicio in `seq 1 2`
+for ejercicio in `seq 1 5`
 do
 	for SEMILLA in `seq 1 5`
 	do
@@ -24,25 +24,25 @@ do
 		if [ $ejercicio -eq 3 ]
 		then
 			echo -e "\nPrimero HoeffdingTree: " >> $ejercicio
-			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio	
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "EvaluateInterleavedTestThenTrain -l moa.classifiers.trees.HoeffdingTree -s (generators.RandomRBFGeneratorDrift -s 0.001 -k 3 -r $SEMILLA -i $SEMILLA -a 7 -n 3) -i 2000000" >> $ejercicio	
 			echo -e "\nAhora HoeffdingAdaptativeTree" >> $ejercicio
-			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "EvaluateInterleavedTestThenTrain -l moa.classifiers.trees.HoeffdingAdaptiveTree -s (generators.RandomRBFGeneratorDrift -s 0.001 -k 3 -r $SEMILLA -i $SEMILLA -a 7 -n 3) -i 2000000" >> $ejercicio
 		fi
 
 		if [ $ejercicio -eq 4 ]
 		then
 			echo -e "\nPrimero HoeffdingTree: " >> $ejercicio
-			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio	
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "EvaluatePrequential -l trees.HoeffdingTree -s (generators.RandomRBFGeneratorDrift -s 0.001 -k 3 -r $SEMILLA -i $SEMILLA -a 7 -n 3) -i 2000000" >> $ejercicio	
 			echo -e "\nAhora HoeffdingAdaptativeTree" >> $ejercicio
-			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "EvaluatePrequential -l trees.HoeffdingAdaptiveTree -s (generators.RandomRBFGeneratorDrift -s 0.001 -k 3 -r $SEMILLA -i $SEMILLA -a 7 -n 3) -i 2000000" >> $ejercicio
 		fi
 
 		if [ $ejercicio -eq 5 ]
 		then
 			echo -e "\nPrimero HoeffdingTree: " >> $ejercicio
-			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio	
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "EvaluateInterleavedTestThenTrain -l (moa.classifiers.drift.SingleClassifierDrift -l trees.HoeffdingTree) -s (generators.RandomRBFGeneratorDrift -s 0.001 -k 3 -r $SEMILLA -i $SEMILLA -a 7 -n 3) -i 2000000" >> $ejercicio	
 			echo -e "\nAhora HoeffdingAdaptativeTree" >> $ejercicio
-			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "" >> $ejercicio
+			java -cp moa-release-2016.04/moa.jar -javaagent:moa-release-2016.04/sizeofag.jar moa.DoTask "EvaluateInterleavedTestThenTrain -l (moa.classifiers.drift.SingleClassifierDrift -l trees.HoeffdingAdaptiveTree) -s (generators.RandomRBFGeneratorDrift -s 0.001 -k 3 -r $SEMILLA -i $SEMILLA -a 7 -n 3) -i 2000000" >> $ejercicio
 		fi
 	done
 done
